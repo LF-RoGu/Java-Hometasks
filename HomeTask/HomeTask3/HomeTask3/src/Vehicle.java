a15b7bd7f5155399ca481d48c5848a716a4e08cd
Luis Fernando <luis_rogu@outlook.com>
2023-10-01 12:43:04 +0200

    change for stream change

--- HomeTask/HomeTask3/HomeTask3/src/Vehicle.java
+++ HomeTask/HomeTask3/HomeTask3/src/Vehicle.java
@@ -1,16 +1,5 @@
 public class Vehicle
 {
-    private Manufacture l_classManufacture;
-    public Vehicle()
-    {
-        System.out.println("Empty constructor called");
-    }
-
-    public Vehicle(Manufacture l_classManufacture)
-    {
-        this.l_classManufacture = l_classManufacture;
-    }
-
     public void ignitionKeyOn()
     {
         System.out.println("Ignition Key On");
@@ -27,30 +16,33 @@ public class Vehicle
 
 class ICEV extends Vehicle
 {
+    private Manufacture l_classManufacture;
     private Engine l_classEngine;
 
     public ICEV(Manufacture l_classManufacture, Engine l_classEngine) {
-        super(l_classManufacture);
+        this.l_classManufacture = l_classManufacture;
         this.l_classEngine = l_classEngine;
     }
 }
 
 class BEV extends Vehicle
 {
+    private Manufacture l_classManufacture;
     private Engine l_classEngine;
 
     public BEV(Manufacture l_classManufacture, Engine l_classEngine) {
-        super(l_classManufacture);
+        this.l_classManufacture = l_classManufacture;
         this.l_classEngine = l_classEngine;
     }
 }
 
 class HybridV extends Vehicle
 {
+    private Manufacture l_classManufacture;
     private Engine l_classEngine;
 
     public HybridV(Manufacture l_classManufacture, Engine l_classEngine) {
-        super(l_classManufacture);
+        this.l_classManufacture = l_classManufacture;
         this.l_classEngine = l_classEngine;
     }
 }
